87f5fde30f9919762de8a0f23beb3393f6949337
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-04-29 22:47:02 -0500

    add validators to form

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import "./NewPlace.css";
 
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
 import Input from "../../shared/components/FormElements/Input";
 
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []); // useCallback -> this should rerender based on dependencies to make useEffect in Input not run again
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <form className="place-form">
-      <Input element="input" type="input" label="Title" />
+      <Input
+        element="input"
+        type="input"
+        id="title"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={titleInputHandler}
+      />
+      <Input
+        element="textarea"
+        label="Description"
+        id="description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)."
+        onInput={descriptionInputHandler}
+      />
     </form>
   );
 };
