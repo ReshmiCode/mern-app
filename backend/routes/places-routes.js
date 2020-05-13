15313c35ae76f8de20bbfb2260c44ace74f0fb5d
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-05-13 14:33:48 -0500

    add a places route

---
+++ backend/routes/places-routes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const router = express.Router();
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "In New York",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.74,
+      lng: -73.98,
+    },
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => p.id === placeId);
+  res.json({ place });
+});
+
+module.exports = router;
