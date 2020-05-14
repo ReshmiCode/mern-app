8b8e0e2f284eece54e08b9db20776e89ef2c2ff2
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-05-13 23:41:16 -0500

    create a place route and error handling

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const HttpError = require("./models/http-error");
 const placesRoutes = require("./routes/places-routes");
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use("/api/places", placesRoutes);
+
+app.use((req, res, next) => {
+  throw new HttpError("Could not find this route.", 404);
+});
 
 app.use((error, req, res, next) => {
   // if any middleware infront of this throws an error, it will run
--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -1,3 +1,5 @@
+const uuid = require("uuid/v4");
+
 const HttpError = require("../models/http-error");
 
 const DUMMY_PLACES = [
@@ -37,5 +39,21 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ place });
 };
 
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+  const createdPlace = {
+    id: uuid(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+
+  DUMMY_PLACES.push(createdPlace);
+  res.status(201).json(createdPlace); // 201 when created data
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
