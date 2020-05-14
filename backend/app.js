b55a2bd6ea996b8c880247a31c4b5f6628062499
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-05-13 20:08:12 -0500

    add error handlers

--- backend/app.js
+++ backend/app.js
@@ -7,4 +7,14 @@ const app = express();
 
 app.use("/api/places", placesRoutes);
 
+app.use((error, req, res, next) => {
+  // if any middleware infront of this throws an error, it will run
+  if (res.headerSent) {
+    // if response already sent
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occured!" });
+});
+
 app.listen(5000);
