842288499ce68081e5bcc8bcf0f4d0dc4c566309
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-05-13 20:32:22 -0500

    use controller for logic

---
+++ backend/controllers/places-controller.js
@@ -0,0 +1,41 @@
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "In New York",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.74,
+      lng: -73.98,
+    },
+    creator: "u1",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  // order matters
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => p.id === placeId);
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id.", 404); // can use throw if not async code
+  }
+  res.json({ place });
+};
+
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => p.creator === userId);
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place for the provided user id.", 404)
+    ); // need to use next(error) with async code
+  }
+  res.json({ place });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,44 +1,11 @@
 const express = require("express");
 
-const HttpError = require("../models/http-error");
+const placesControllers = require("../controllers/places-controller");
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "In New York",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lat: 40.74,
-      lng: -73.98,
-    },
-    creator: "u1",
-  },
-];
+router.get("/:pid", placesControllers.getPlaceById);
 
-router.get("/:pid", (req, res, next) => {
-  // order matters
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => p.id === placeId);
-  if (!place) {
-    throw new HttpError("Could not find a place for the provided id.", 404); // can use throw if not async code
-  }
-  res.json({ place });
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => p.creator === userId);
-  if (!place) {
-    return next(
-      new HttpError("Could not find a place for the provided user id.", 404)
-    ); // need to use next(error) with async code
-  }
-  res.json({ place });
-});
+router.get("/user/:uid", placesControllers.getPlaceByUserId);
 
 module.exports = router;
